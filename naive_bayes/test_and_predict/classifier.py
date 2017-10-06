95a88aeb0f8c5bc0471c40a3b49cee9fff2c4cb0
kessler-frost <sankalp.sanand@gmail.com>
2017-10-07 01:10:08 +0530

    Models tweaked and trained again.

--- naive_bayes/test_and_predict/classifier.py
+++ naive_bayes/test_and_predict/classifier.py
@@ -1,7 +1,7 @@
 import trained_models as models
 from statistics import mode
-from sklearn.metrics import accuracy_score
-#import pickle
+from sklearn.metrics import accuracy_score, confusion_matrix
+import pickle
 import get_testing_data_script as gt
 import numpy as np
 
@@ -9,19 +9,19 @@ def predict(features):
     predicted_labels = []
 
     predicted_labels.append(models.GNB(features))
-    predicted_labels.append(models.BNB(features))
-    predicted_labels.append(models.MNB(features))
+    # predicted_labels.append(models.BNB(features))
+    # predicted_labels.append(models.MNB(features))
     predicted_labels.append(models.SGDC(features))
 
-    return mode(predicted_labels)
+    return predicted_labels#, mode(predicted_labels)
 
-features = gt.get_test_features()[0]
-print(predict(features))
+test_features = gt.get_test_features()
+test_labels = gt.get_test_labels()
 
 # list_of_priors = []
-# with open("final/pickle_jar/model_gnb.pkl", "rb") as f:
+# with open("naive_bayes/pickle_jar/model_gnb.pkl", "rb") as f:
 #     model = pickle.load(f)
-#     list_of_priors.append(model.class_prior_)
+#     print(model.score(np.array(test_features), np.array(test_labels)))
 #
 # with open("final/pickle_jar/model_bnb.pkl", "rb") as f:
 #     model = pickle.load(f)
--- naive_bayes/test_and_predict/get_testing_data_script.py
+++ naive_bayes/test_and_predict/get_testing_data_script.py
@@ -4,7 +4,7 @@ def get_test_features():
     for row in open('naive_bayes/test_and_predict/test_data.csv', 'r').read().strip().split("\n"):
         values_list = []
         for values in row.split(",")[1:]:
-            values_list.append(float(values))
+            values_list.append(abs(float(values)))
         features.append(values_list)
 
     return features
